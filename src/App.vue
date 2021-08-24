c436b559e5a41b82de89b0fe53c33afecd7b8d8c
aymensp <aymen.smati@esprit.tn>
2021-08-24 16:55:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-switch
+      v-model="mode"
+      active-text="Part 2"
+      inactive-text="Part1 and Part3"
+      size="mini"
+      style="margin-bottom:10vh;">
+  </el-switch>
+  <PeterNumber v-if="mode"/>
+  <Calculator  v-else />
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+import PeterNumber from './components/PeterNumber.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator,
+    PeterNumber
+  },
+  data () {
+    return {
+      mode: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
